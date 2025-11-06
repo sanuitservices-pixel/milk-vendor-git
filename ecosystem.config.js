de3f759ab9164333b965d3faeeb68856ccedb9d8
unknown <sanuitservices@gmail.com>
2025-11-06 19:56:22 +0530

    config file update

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -13,7 +13,7 @@ module.exports = {
             "user": "ubuntu",
             "host": "56.155.114.153",
             "ref": "origin/master",
-            "repo": "git@bitbucket.org:dairy-inventory/milk-vendor.git ",
+            "repo": "https://github.com/sanuitservices-pixel/milk-vendor-git.git",
             "path": "/home/ubuntu/milk_vendor_api",
             "pre-deploy": "echo 'This is executed before deployment'",
             "post-deploy": "cp ../.env ./ && npm install && pm2 startOrRestart ecosystem.config.js --env production"
